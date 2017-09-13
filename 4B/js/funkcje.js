f827cd18b06bbda9472b2822981ee14c38a0595f
szyper <zskgit@gmail.com>
2017-09-13 12:19:07 +0200

    Funkcje

--- 4B/js/funkcje.js
+++ 4B/js/funkcje.js
@@ -6,14 +6,24 @@ var name = 'Janusz';
 witaj(name);
 
 
-function poleProstokata(){
+/*function poleProstokata(){
     var elSzerokosc = document.getElementById('szerokosc').value;
     var elDlugosc = document.getElementById('dlugosc').value;
     var elWyswietl = document.getElementById('wyswietl');
     elWyswietl.innerHTML = elSzerokosc * elDlugosc;
+}*/
+
+
+
+    var elPrzycisk = document.getElementById('przycisk');
+
+    elPrzycisk.onclick = function (elDlugosc,elSzerokosc){
+    var elSzerokosc = document.getElementById('szerokosc').value;
+    var elDlugosc = document.getElementById('dlugosc').value;
+    var elWyswietl = document.getElementById('wyswietl');
+    var pole = elDlugosc * elSzerokosc;
+    elWyswietl.innerHTML = pole + 'cm<sup>2</sup>'
 }
-
-
 
 
 
