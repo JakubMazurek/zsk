1354127268e4b404c9f1fe8d68a2f7aea1998e74
szyper <zskgit@gmail.com>
2017-09-15 15:11:10 +0200

    Math

--- 4B/js/funkcje.js
+++ 4B/js/funkcje.js
@@ -26,6 +26,30 @@ witaj(name);
 }
 
 
+    function poleObjetosc(szerokosc, dlugosc, wysokosc){
+        var pole = szerokosc * dlugosc;
+        var objetosc = pole * wysokosc;
+        var wynik = [pole, objetosc];
+        return wynik;
+    }
+
+    console.log(poleObjetosc(2,3,4));
+    var pole = poleObjetosc(2,3,10)[0];
+    var objetosc = poleObjetosc(2,3,10)[1];
+    console.log(pole);
+    console.log(objetosc);
+
+    var x = prompt('Podaj wartość','0 - pole, 1 - objetość');
+    console.log(poleObjetosc(1,2,3)[x]);
+
+    console.log(Math.PI);
+
+   /* zad. dom. napisać skrypt, który oblicza obwód, pole oraz objętośćstożka. Dane użytkownik podaje z klawiatury. Wykorzystaj obiekt Math. Użytkownik wybiera za pomocąpola radio co chce obliczyć. Dane wprowadza w formularzu. Wynik wyświetl w bloku.*/
+
+
+
+
+
 
 
 
