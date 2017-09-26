fcc73dda1a9b356628aead7919439c004c6c2e71
szyper <zskgit@gmail.com>
2017-09-26 10:07:21 +0200

    4TE FizzBuzz

--- 4E/js/dostepDoElementow.js
+++ 4E/js/dostepDoElementow.js
@@ -70,7 +70,8 @@ elPrzycisk.onclick = function (){
 }
 
 
-
+/*FizzBuzz
+Zadanie brzmi: Napisz program, który wyświetla liczby od 1 do 100. Dla liczb podzielnych przez 3 niech program wyświetli „Fizz”; dla liczb podzielnych przez 5 niech wyświetli ‚Buzz’; a dla liczb podzielnych przez 15 niech wyświetli ‚FizzBuzz’.*/
 
 
 
