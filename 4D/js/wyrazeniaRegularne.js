acefb72fe1e14840bc85c210a75e9eca5bcf08f5
szyper <zskgit@gmail.com>
2017-09-26 15:42:25 +0200

    4TD

---
+++ 4D/js/wyrazeniaRegularne.js
@@ -0,0 +1,55 @@
+//Wyrażenia regularne
+
+var text = 'Zespół Szkół Komunikacji';
+var regex = /szkół/i;
+var sprawdz = regex.test(text);
+
+var text1 = 'Technik informatyk';
+//var regex1 = /informatyk$/;     //true
+//var regex1 = /^informatyk/;     //false
+//var regex1 = /^informatyk$/;     //false
+//var regex1 = /Tech[a-z]/;     //true
+//var regex1 = /Tech[a-z]?/;     //true
+//var regex1 = /Tech[a-z]*/;     //true
+//var regex1 = /Tech[a-z]{1}/;     //true
+//var regex1 = /Tech[a-z]{1,2}/;     //true
+//var regex1 = /Tech[a-z]{1,}/;     //true
+//var regex1 = /Tech[a-z\s]{1}/;     //true
+//var regex1 = /Tech[a-z\s]{1,3}/;     //true
+//var regex1 = /[a-z]+/;     //true
+//var regex1 = /[a-z]+/g;     //true
+//var regex1 = /[a-z]+/gi;     //true
+//var regex1 = /[a-z]+/gi;     //true
+var regex1 = /([a-z]+)/gi;     //true
+
+var sprawdz1 = regex1.test(text1);
+console.log(sprawdz1);
+
+//kod pocztowy
+var regex2 = /[0-9]{2}-[0-9]{3}/;
+var regex2 = /\d{2}-\d{3}/;
+
+function sprawdzKod (){
+    var text = document.getElementById('kod').value;
+    var blok = document.getElementById('blok');
+    var regex3 = /\d{2}-\d{3}/;
+    var sprawdz = regex3.test(text);
+    if (sprawdz == true){
+        blok.style.color = 'blue';
+        blok.textContent = 'Prawidłowe dane';
+    }else{
+        blok.style.color = 'red';
+        blok.textContent = 'Błędne dane';
+        text.value = '';
+    }
+}
+
+var przycisk = document.getElementById('przycisk');
+
+przycisk.addEventListener('click', sprawdzKod);
+
+
+
+
+
+
--- 4D/js/zdarzenia.js
+++ 4D/js/zdarzenia.js
@@ -38,13 +38,13 @@ blok.style.color = 'red';
 
 
 function sprawdz(){
-    var element = document.getElementById(this.id);
+    //var element = document.getElementById(this.id);
     //console.log(element);
-    if (element.value.length  >2 && element.value.length < 10){
+    if (this.value.length  >2 && this.value.length < 10){
         blok.textContent = '';
     }else{
         blok.textContent = 'Błęde dane!';
-        element.focus();
+        this.focus();
     }
 }
 
@@ -55,17 +55,86 @@ function mail (){
         mail1.value = '';
         mail2.value = '';
         mail1.focus();
+        mail2.disabled = true;
     }else{
-
+        mail1.disabled = true;
+        mail2.disabled = true;
+        blok.textContent = '';
     }
 }
+
+
+function pass (){
+    if (pass1.value != pass2.value){
+        blok.textContent = 'Hasła są różne!';
+        pass1.value = '';
+        pass2.value = '';
+        pass1.focus();
+        pass2.disabled = true;
+    }else{
+        pass1.disabled = true;
+        pass2.disabled = true;
+        blok.textContent = '';
+    }
+}
+
+
+function blokuj (){
+
+    mail2.disabled = false;
+    mail2.focus();
+}
+
+function blokujPass (){
+
+    pass2.disabled = false;
+    pass2.focus();
+}
+
+function sprawdz (){
+    if(regulamin.checked==true){
+        przycisk.disabled = false;
+    }else{
+        przycisk.disabled = true;
+    }
+
+}
+
+
+function odblokuj (){
+    var blokowane = document.getElementsByTagName('input');
+    if (blokowane.length > 0){
+    for (var i=0; i<blokowane.length; i++){
+        //console.log(blokowane[i]);
+        if(blokowane[i].disabled == true){
+            blokowane[i].disabled = false;
+        }
+}
+    }
+}
+
+function wyslij (){
+    document.write('<div>');
+    document.write('Imię: ' + imie.value);
+    document.write('<br>Nazwisko: ' + nazwisko.value);
+    document.write('<br>Login: ' + login.value);
+    document.write('<br>Mail: ' + mail1.value);
+    document.write('<br>Data urodzenia: ' + data.value);
+    document.write('</div>');
+}
+
 
 imie.addEventListener('blur', sprawdz);
 nazwisko.addEventListener('blur', sprawdz);
 login.addEventListener('blur', sprawdz);
+mail1.addEventListener('blur', blokuj);
+pass1.addEventListener('blur', blokujPass);
 mail2.addEventListener('blur', mail);
-
-
+pass2.addEventListener('blur', pass);
+//regulamin.addEventListener('click', sprawdz);
+regulamin.addEventListener('change', sprawdz);
+popraw.addEventListener('click', odblokuj);
+przycisk.addEventListener('click', wyslij);
 
 
 
