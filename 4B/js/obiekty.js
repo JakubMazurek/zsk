92a59bf413188b73abdb54f873ffc4444253e658
szyper <zskgit@gmail.com>
2017-10-06 15:33:19 +0200

    4TB zadanie domowe

--- 4B/js/obiekty.js
+++ 4B/js/obiekty.js
@@ -114,10 +114,124 @@ var osoba1 = new Osoba('Janusz');
 var osoba2 = new Osoba('Oliwia');
 
 var daneOsoba1 = osoba1;
-console.log(daneOsoba1);
+//console.log(daneOsoba1);
 
 //zrobić zadanie z hotelem!!!!!!!!!!!
 
+var hotel = new Object();
+hotel.nazwa = 'Hotel Poznań';
+hotel.pokoje = 100;
+hotel.pokojeZarezerwowane = 31;
+hotel.silownia = true;
+hotel.rodzajePokoi = ['pojedynczy', 'podwojny', 'apartament'];
+hotel.pokojeWolne = function() {
+    return this.pokoje - this.pokojeZarezerwowane;
+}
+
+//console.log(hotel.pokojeWolne());
 
 
+//*****************************************************************
 
+//var tablica = new Array();
+
+function Hotel(nazwa, pokoje, pokojeZarezerwowane){
+    this.nazwa = nazwa;
+    this.pokoje = pokoje;
+    this.pokojeZarezerwowane = pokojeZarezerwowane;
+    this.wolnePokoje = function() {
+        return this.pokoje - this.pokojeZarezerwowane;
+    }
+};
+
+var lech = new Hotel('Lech', 45, 30);
+var mercury = new Hotel('Mercury', 450, 248);
+
+//document.write('Wolne pokoje w hotelu Lech: ' + lech.wolnePokoje() + '<br>');
+//document.write('Wolne pokoje w hotelu Maercury: ' + mercury.wolnePokoje());
+
+//*******************************************************************************
+
+/*zad. utwórz konstruktor o nazwie pies i parametrach: imie, rasa, waga oraz ulubioneZajecia, które mogą mieć wiele wartości. Utwótz 3 obiekty o nazwach: azor, edi, saba.*/
+
+function Pies(imie, rasa, waga, ulubioneZajecia){
+    this.imie = imie;
+    this.rasa = rasa;
+    this.waga = waga;
+    this.ulubioneZajecia = ulubioneZajecia;
+}
+
+var azor = new Pies('Azor', 'bokser', 30, ['bieganie', 'aportowanie']);
+var edi = new Pies('Edi', 'bokser', 7, ['bieganie', 'aportowanie']);
+var saba = new Pies('Saba', 'bokser', 10, ['bieganie', 'aportowanie']);
+
+var psy = [azor, edi, saba];
+
+edi.ulubioneZajecia.push('spanie');
+
+//console.log(edi.ulubioneZajecia);
+//console.log(psy);
+
+var rozmiar;
+for (var i=0; i<psy.length; i++){
+    if (psy[i].waga > 10){
+       rozmiar  = 'duży pies';
+    }else{
+        rozmiar = 'mały pies';
+    }
+    //document.write('Pies: ' + '<span style="color:blue">' + psy[i].imie + '</span>' + ' to ' + rozmiar + '<br>');
+}
+
+//********************************************************************
+
+
+var tab = ['Jan', 'Nowak', 'Poznań'];
+for (var i in tab){
+   //document.write(tab[i]);
+}
+
+/*for (var x in edi){
+    document.write(edi[x] + '<br>');
+}*/
+
+
+/*for (var x in edi){
+    if (x == 'imie' || x == 'rasa')
+    //document.write(x + '<br>');
+    document.write(edi[x] + '<br>');
+}*/
+
+//**************************************************************
+//**************************************************************
+
+//prototype
+
+function Pole(a, b){
+    return a*b;
+}
+
+//console.log(Pole.length)
+//console.log(Pole.constructor);
+//console.log(Pole.prototype);
+
+//*******************************************
+
+function Uczen(imie, nazwisko){
+    this.imie = imie;
+    this.nazwisko = nazwisko;
+}
+
+var adrian = new Uczen('Adrian', 'Nowak');
+Uczen.prototype.narodowosc = 'polska';
+Uczen.prototype.wyswietlUcznia = function() {
+    return 'Imię: ' + this.imie + ', nazwisko: ' + this.nazwisko + ', narodowość: ' + this.narodowosc;
+}
+
+//document.write(adrian.wyswietlUcznia());
+console.log(adrian.constructor);
+console.log(adrian.constructor.prototype.narodowosc);
+console.log(adrian.hasOwnProperty('imie')); //true
+console.log(adrian.hasOwnProperty('narodowosc')); //false
+
+/*zad.
+Utwórz konstruktor rower w którym będą właściwości: marka, kolor. Wyświetl wszystkie dane za pomocą metody wyswietlRower. Wykorzystaj for in. Utwórz prototyp w którym będzie zapisana ilość kół.*/
