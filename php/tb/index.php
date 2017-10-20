222d647fee29f25702119679ae3a01a59b913a6b
szyper <zskgit@gmail.com>
2017-10-20 15:22:36 +0200

    4TB

--- php/tb/index.php
+++ php/tb/index.php
@@ -1,3 +1,60 @@
-<?php
-    echo ("4TB");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Zajęcia2</title>
+</head>
+<body>
+    <?php //znacznik kanonicnzy
+        echo ("4TB");
+    ?>
+
+    <?  //znacznik typu SGML
+        echo "Skrócony";
+    ?>
+
+    <!-- Znacznik typu ASP nie wykorzystywany w php7<%
+
+    %>-->
+
+
+    <!--<script language="php"></script>-->
+
+    <?php
+        //komentarz jednowierszowy
+
+    /*
+    komentarz w
+    wielu liniach
+    */
+
+    #komentarz jednowierszowy
+
+    //wyświetlanie informacji
+
+    echo ("cudzysłów<br>");
+    echo ('apostrof<br>');
+    echo "tekst";
+    echo '4TB';
+    echo 18;
+    echo "<br>Masz 18 lat";
+    echo "<br>Masz ", 18, " lat";
+
+    echo "<h2>Nagłówek</h2>";
+    //echo "<h2 style="color:blue">"; //błąd
+    //echo "<h2 style=\"color:blue\">Weekend</h2>";
+    echo '<h2 style="color:blue">Weekend</h2>';
+
+    //print "egzamin";
+    //print "egzamin",18;   //Parse error: syntax error
+
+
+    ?>
+
+</body>
+
+
+</html>
+
+
+
---
+++ php/tb/main/main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Zajęcia2</title>
+</head>
+<body>
+    <?php
+        echo '<h2 style="text-align: center">';
+            echo "Witamy na stronie!";
+        echo '</h2>';
+        //include "../imie.php";
+        //include "../imie1.php";
+        //@include "../imie1.php";
+
+        //require "../imie.php";
+        //require "../imie1.php";
+        //@require "../imie1.php";
+
+        //echo "<br>Tekst po błędzie";
+    ?>
+
+
+</body>
+
+
+</html>
+
+
+
