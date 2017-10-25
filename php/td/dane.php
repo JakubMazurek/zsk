43cb12d1f718570198ed2567b209d21005ff2376
szyper <zskgit@gmail.com>
2017-10-25 15:43:46 +0200

    4td

--- php/td/dane.php
+++ php/td/dane.php
@@ -1,21 +1,21 @@
 <?php
-   /* $imie = $_GET['imie'];
+    $imie = $_GET['imie'];
     $nazwisko = $_GET['nazwisko'];
     //echo $imie, $nazwisko;
 
-    if (empty($imie) || empty($nazwisko)){
+/*    if (empty($imie) || empty($nazwisko)){
         echo 'Nie wprowadziłeś poprawnie danych';
     }else{
         echo "Twoje imię: <span style=\"color:red\">$imie</span><br>";
         echo "Twoje nazwisko:<span style=\"color:red\"> $nazwisko</span><br>";
     }*/
 
-   /* if (isset($_GET['radio1'])){
+    if (isset($_GET['radio1'])){
         //echo "Zaznaczyłeś wartość: {$_GET['radio1']}";
         echo "Zaznaczyłeś wartość: ", $_GET['radio1'];
     }else{
         //echo "Zaznacz jedną z opcji";
         header('location: formularz.php');
-    }*/
+    }
 
 ?>
