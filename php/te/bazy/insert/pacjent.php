b35a23751a9bc705be30e871fd16071f241722dc
szyper <zskgit@gmail.com>
2017-11-10 13:13:33 +0100

    4TE php bazy  select , insert, delete, sort

--- php/te/bazy/insert/pacjent.php
+++ php/te/bazy/insert/pacjent.php
@@ -8,12 +8,74 @@
 <body>
     <h2>Wyświetlanie i dodawanie danych</h2>
 
-    <?php
+       <?php
+
+        $polaczenie = @new mysqli('localhost', 'root', '', 'przychodniate');
+
+        if(!$polaczenie->connect_error){
+
+            $polaczenie->set_charset("utf8");
+
+            $sort = 'id';
+
+            if(isset($_GET['sort'])){
+                $sort = $_GET['sort'];
+            }
+
+            $sql = "SELECT * FROM pacjenci ORDER BY $sort";
+
+            echo <<<TABELA
+            <table>
+                <tr>
+                    <th><a href='pacjent.php?sort=id'>id</a></th>
+                    <th><a href='pacjent.php?sort=imie'>imie</a></th>
+                    <th><a href='pacjent.php?sort=nazwisko'>nazwisko</a></th>
+                    <th><a href='pacjent.php?sort=choroby_przewlekle'>choroby przewlekłe</a></th>
+                    <th><a href='pacjent.php?sort=uczulenia'>uczulenia</a></th>
+                    <th><a href='pacjent.php?sort=choroba'>choroba</a></th>
+                    <th><a href='pacjent.php?sort=leki_przepisane'>leki przepisane</a></th>
+                    <th><a href='pacjent.php?sort=opis'>opis</a></th>
+                    <th>Usuń</th>
+                </tr>
+TABELA;
+
+            if($rezultat = $polaczenie->query($sql)){
+
+                while($wiersz = $rezultat->fetch_assoc()){
+
+                    echo <<<WIERSZ
+                    <tr>
+                    <td>{$wiersz['id']}</td>
+                    <td>{$wiersz['imie']}</td>
+                    <td>{$wiersz['nazwisko']}</td>
+                    <td>{$wiersz['choroby_przewlekle']}</td>
+                    <td>{$wiersz['uczulenia']}</td>
+                    <td>{$wiersz['choroba']}</td>
+                    <td>{$wiersz['leki_przepisane']}</td>
+                    <td>{$wiersz['opis']}</td>
+                    <td><a href='delete.php?id={$wiersz['id']}'>usuń</a></td>
+                    </tr>
+WIERSZ;
+
+                }
 
 
+            echo "</table>";
 
+            }else{
+                echo "Błędne zapytanie";
+            }
+
+        }else{
+            echo "Błąd połączenia: ".$polaczenie->connect_errno;
+        }
+
+    echo "<br>";
+
+    $polaczenie->close();
 
     ?>
+
 
     <form action="insert.php" method="post">
         <input type="text" name="imie" placeholder="imię"><br><br>
@@ -22,6 +84,10 @@
         <input type="text" name="uczulenia" placeholder="uczulenia"><br><br>
         <input type="text" name="choroba" placeholder="choroba"><br><br>
         <input type="text" name="leki_przepisane" placeholder="leki przepisane"><br><br>
+
+        <textarea rows="6" cols="25" name="opis">opis...
+        </textarea><br><br>
+
         <input type="submit" name="przycisk" value="Dodaj"><br><br>
     </form>
 
