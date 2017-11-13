c10938bdc484f0c0bf00bea0d09303139b83c783
poznanprogramowanie <irobotyka@gmail.com>
2017-11-13 12:57:16 +0100

    4TD egzamin 3 , COOKIE

---
+++ php/td/bazy/egzamin3/error.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Aktualizacja nie powiodła się";
+?>
---
+++ php/td/cookie/cookie3_licznik.php
@@ -0,0 +1,33 @@
+<?php
+    $dni = 30;
+
+    if(!isset($_COOKIE['licznik'])){
+        $licznik = 1;
+    }else{
+        $licznik = intval($_COOKIE['licznik']);
+        $licznik++;
+    }
+
+    setcookie('licznik', "$licznik", time() + 60*60*24*$dni);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Licznik</title>
+</head>
+<body>
+    <?php
+        if($licznik == 1){
+            echo "Liczba odwiedzin w ciągu ostatnich $dni dni:
+            <span style=\"color:red\">$licznik </span>raz";
+        }else{
+            echo "Liczba odwiedzin w ciągu ostatnich $dni dni:
+            <span style=\"color:red\">$licznik </span>razy";
+        }
+
+
+    ?>
+</body>
+</html>
