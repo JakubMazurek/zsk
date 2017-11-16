0fe46ce98495a60ab88bd9a162802aeb34f25f9f
szyper <zskgit@gmail.com>
2017-11-16 11:25:25 +0100

    4TC_COOKIE

---
+++ php/tc/cookie/cookie3_licznik.php
@@ -0,0 +1,46 @@
+<?php
+   $dni = 30;
+
+    if(!isset($_COOKIE['licznik'])){
+        $licznik = 1;
+    }else{
+        $licznik = intval($_COOKIE['licznik']);
+        $licznik++;
+    }
+
+    setcookie('licznik', $licznik, time()+60*60*24*$dni);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Cookie</title>
+</head>
+<body>
+    <?php
+       if($licznik == 1){
+           echo "Liczba odwiedzin w ciągu ostatnich $dni dni:
+           <span style=\"color:red\">$licznik</span> raz";
+       }else{
+           echo "Liczba odwiedzin w ciągu ostatnich $dni dni:
+           <span style=\"color:red\">$licznik</span> razy";
+       }
+    ?>
+
+    <form action="./cookie3_licznik.php">
+        <input type="submit" name="usun" value="Usuń ciasteczko">
+    </form>
+
+    <?php
+        if(isset($_GET['usun'])){
+            setcookie('licznik', false);
+            header('location: ./cookie3_licznik.php');
+        }
+    ?>
+
+
+
+
+</body>
+</html>
---
+++ php/tc/sesja/sesja1.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    $_SESSION['imie'] = "Filip";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Wiek</title>
+</head>
+<body>
+</body>
+</html>
